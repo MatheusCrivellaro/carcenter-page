72ca533af7d177a7e0c2d2676b258d83334e46b7
MatheusCrivellaro <matheuscriv@gmail.com>
2024-09-06 23:56:16 -0300

    atualizando carrossel de banners

--- src/components/CarouselBanner/CarouselBanner.tsx
+++ src/components/CarouselBanner/CarouselBanner.tsx
@@ -1,28 +1,52 @@
 import './CarouselBanner.css'
+import {Swiper, SwiperSlide} from "swiper/react";
+import {SwiperOptions} from "swiper/types";
+import {useRef} from "react";
+import {MdArrowBackIosNew, MdArrowForwardIos} from "react-icons/md";
 
 const CarouselBanner = () => {
+
+    const settingsSwiper: SwiperOptions = {
+        slidesPerView: 1,
+        loop: true,
+        grabCursor: true,
+        autoplay: {delay: 6000}
+    };
+
+    const swiperRef = useRef<any>(null);
+
     return (
-        <div id="carouselExample" className="carousel slide carousel-banner">
-            <div className="carousel-inner carousel-div-banner">
-                <div className="carousel-item active div-banners-carousel">
-                    <img src="https://resized-images.autoconf.com.br/1440x338/filters:format(webp)/site/912/72a94062-ce7f-4d25-8dbf-a81ea86733bf.jpeg" alt=""/>
-                </div>
-                <div className="carousel-item div-banners-carousel">
-                    <img src="https://resized-images.autoconf.com.br/1440x338/filters:format(webp)/site/912/5d449fb9-40e3-4dff-949f-2ea47ef1a867.png" alt=""/>
-                </div>
-                <div className="carousel-item div-banners-carousel">
-                    <img src="https://resized-images.autoconf.com.br/1440x338/filters:format(webp)/site/912/be907c1b-c8d0-4d69-811a-cbf5fe8424b6.jpeg" alt=""/>
-                </div>
+        <Swiper
+            {...settingsSwiper}
+            onSwiper={(swiper) => (swiperRef.current = swiper)}
+            id="carouselExample" className="carousel-banner"
+        >
+            <SwiperSlide className="div-banners-carousel">
+                <img
+                    src="https://resized-images.autoconf.com.br/1440x338/filters:format(webp)/site/912/72a94062-ce7f-4d25-8dbf-a81ea86733bf.jpeg"
+                    alt=""/>
+            </SwiperSlide>
+            <SwiperSlide className="div-banners-carousel">
+                <img
+                    src="https://resized-images.autoconf.com.br/1440x338/filters:format(webp)/site/912/5d449fb9-40e3-4dff-949f-2ea47ef1a867.png"
+                    alt=""/>
+            </SwiperSlide>
+            <SwiperSlide className="div-banners-carousel">
+                <img
+                    src="https://resized-images.autoconf.com.br/1440x338/filters:format(webp)/site/912/be907c1b-c8d0-4d69-811a-cbf5fe8424b6.jpeg"
+                    alt=""/>
+            </SwiperSlide>
+            <div className="carousel-banner-button carousel-banner-button-prev">
+                <button onClick={() => swiperRef.current.slidePrev()}>
+                    <MdArrowBackIosNew/>
+                </button>
             </div>
-            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
-                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span className="visually-hidden">Previous</span>
-            </button>
-            <button className="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
-                <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                <span className="visually-hidden">Next</span>
-            </button>
-        </div>
+            <div className="carousel-banner-button carousel-banner-button-next">
+                <button onClick={() => swiperRef.current.slideNext()}>
+                    <MdArrowForwardIos/>
+                </button>
+            </div>
+        </Swiper>
     )
 }
 
